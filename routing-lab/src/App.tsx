ff8dae3102514c02c23faca2c2ff404b34f3380f
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-06 16:48:10 +0900

    created layout and error element

--- routing-lab/src/App.tsx
+++ routing-lab/src/App.tsx
@@ -1,3 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./pages/common/Layout";
+import NotFound from "./pages/common/NotFount";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        errorElement: <NotFound />,
+        // children:
+    },
+]);
+
 export default function App() {
-    return null;
+    return <RouterProvider router={router} />;
 }
--- routing-lab/src/index.tsx
+++ routing-lab/src/index.tsx
@@ -1,8 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
+import ReactDOM from "react-dom/client";
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+import { ChakraProvider } from "@chakra-ui/react";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  body{ 
+    background-color: #121212;
+    color: whitesmoke;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  * {
+    box-sizing: border-box;
+    overflow: hidden;
+  }
+`;
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+    <>
+        <GlobalStyle />
+        <ChakraProvider>
+            <App />
+        </ChakraProvider>
+    </>
+);
