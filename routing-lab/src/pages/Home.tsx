c2c5add1bad272b4abc7896b3d7a793f6bf2a7b8
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2024-03-06 20:53:03 +0900

    added profile component

--- routing-lab/src/pages/Home.tsx
+++ routing-lab/src/pages/Home.tsx
@@ -1,14 +1,25 @@
-import { Center } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import Album from "../components/album/Album";
+import { AUTHORS } from "../global/data";
+import Profile from "../components/profile/Profile";
 
 export default function Home() {
+    const authors = AUTHORS;
+    const logos = [
+        "/resources/images/logo/HarryPoter_Logo.png",
+        "/resources/images/logo/LoadOfRing.png",
+    ];
+
     return (
-        <Center w="100%">
-            <Album
-                width={440}
-                height={620}
-                image="https://tcg.pokemon.com/assets/img/global/tcg-card-back-2x.jpg"
-            />
-        </Center>
+        <HStack w="100%" justifyContent="center" mt="50px" spacing="50px">
+            {authors.map((author, index) => (
+                <Profile
+                    key={author.name}
+                    name={author.name}
+                    logoImage={logos[index]}
+                    profileImage={author.profileImage}
+                />
+            ))}
+        </HStack>
     );
 }
